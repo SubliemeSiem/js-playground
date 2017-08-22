6985551a28eea8831c098ee0457ec00bec6c6bae
SubliemeSiem <sublieme.siem@gmail.com>
2017-08-22 20:50:24 +0200

    Added comments

--- clientScripts/ajax.js
+++ clientScripts/ajax.js
@@ -2,6 +2,7 @@ const ajax = (function() {
     "use strict";
     return {
         get: function(url, data) {
+            // we return a promise for nice callback handling
             return new Promise(function(resolve, reject) {
                 const xhr = new XMLHttpRequest();
                 const query = [];
@@ -21,12 +22,16 @@ const ajax = (function() {
             });
         },
         post: function(url, data, contentType) {
+            // we return a promise for nice callback handling
             return new Promise(function(resolve, reject) {
+                // if no contentType is set we use json
                 contentType = contentType || 'application/json';
 
                 xhr = new XMLHttpRequest();
                 xhr.open('POST', url, true);
                 xhr.setRequestHeader('Content-Type', contentType);
+
+                // when we get a response we handle the response
                 xhr.onload = function() {
                     if (xhr.status === 200) {
                         resolve(xhr);
@@ -36,16 +41,19 @@ const ajax = (function() {
                 };
 
                 if (contentType === 'application/json') {
+                    // encode data as json
                     xhr.send(JSON.stringify(data));
                 } else if (contentType === 'application/x-www-form-urlendcoded') {
+                    // encode data as URI query
                     const query = [];
-
+                    
                     for (const key in data) {
                         query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
                     }
-
+                    
                     xhr.send(encodeURI(query.join('&')));
                 } else {
+                    // send data 'as is'
                     xhr.send(data);
                 }
             });
