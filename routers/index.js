ba0d5e6586298ef6c4da62db17efc196b47a48e8
SubliemeSiem <sublieme.siem@gmail.com>
2017-08-23 18:12:21 +0200

    Changed MVC configuration to be more straight-forward

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,8 @@
 module.exports = function(rootDir){
     "use strict";
+    const build = function(view, model){
+        return view.split(/\{\{|\}\}/).map((x, index) => index % 2 === 1 && model[x] ? model[x] : x).join('');
+    }
     const router = require('express').Router();
     const fs = require('fs');
     const path = require('path');
@@ -7,7 +10,8 @@ module.exports = function(rootDir){
     const model = require(path.join(rootDir, 'models/index'));
     router.get('/', function(req, res){
         // replace keys in template with values from the model
-        const page = model.reduce((prev, curr) => prev.replace(curr.key, curr.value), view);
+        //const page = model.reduce((prev, curr) => prev.replace(curr.key, curr.value), view);
+        const page = build(view, model);
         if (page){
             res.status(200).send(page);
         } else {
